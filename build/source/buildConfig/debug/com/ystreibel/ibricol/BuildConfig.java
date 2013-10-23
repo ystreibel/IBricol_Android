e850d1c67184fc52bd5e085ceac6056462572bc1
Streibel <ykaribou@gmail.com>
2013-10-24 01:05:56 +0200

    [DEV] Add graddle configuration for android studio

---
+++ build/source/buildConfig/debug/com/ystreibel/ibricol/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ystreibel.ibricol;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
